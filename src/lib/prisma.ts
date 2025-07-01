0f852f1754ead63073b6f99469875ffad6901b90
Robel Asaminew <robelas2001@gmail.com>
2025-07-01 13:20:55 +0000

    PrismaClientInitializationError: Invalid `{imported module [project]/sr

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting the database connection limit.
-//
-// Learn more: https://pris.ly/d/help/next-js-best-practices
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
 declare global {
-  // allow global `var` declarations
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-const prisma = global.prisma ?? new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
-}
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
