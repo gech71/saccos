9a1f37c6172827af4086b0a6a1d79d9fab162483
Hawi Tulu <hawitulu71@gmail.com>
2025-07-23 07:14:05 +0000

    npx prisma db seed Environment variables loaded from .env Running seed c

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -27,7 +27,6 @@ async function main() {
   await prisma.shareType.deleteMany();
   await prisma.savingAccountType.deleteMany();
   await prisma.school.deleteMany();
-  await prisma.building.deleteMany();
   await prisma.user.deleteMany();
   await prisma.role.deleteMany();
   console.log('Database cleaned.');
