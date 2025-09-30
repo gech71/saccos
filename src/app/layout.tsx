49e9707527fc5e5963ace9e988b198a9b795ab77
Hawi Tulu <hawitulu71@gmail.com>
2025-09-30 15:30:19 +0000

    Error: Route "/dashboard" used `headers().get('x-nonce')`. `headers()` s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,6 +90,7 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <style
+          nonce={nonce}
           dangerouslySetInnerHTML={{
             __html: `
               :root {
